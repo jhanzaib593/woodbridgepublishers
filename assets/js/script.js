93fb48b4489328ca262082d56d7c02077e44b4fa
Jhanzaib <jhanzaib593@gmail.com>
2024-03-12 11:17:56 +0500

    change

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,41 @@
-const counters = document.querySelectorAll(".count");
-const speed = 200;
+(function ($) {
+  "use strict";
 
-counters.forEach((counter) => {
-  const updateCount = () => {
-    const target = parseInt(+counter.getAttribute("data-target"));
-    const count = parseInt(+counter.innerText);
-    const increment = Math.trunc(target / speed);
-    console.log(increment);
-
-    if (count < target) {
-      counter.innerText = count + increment;
-      setTimeout(updateCount, 1);
-    } else {
-      count.innerText = target;
-    }
+  var fullHeight = function () {
+    $(".js-fullheight").css("height", $(window).height());
+    $(window).resize(function () {
+      $(".js-fullheight").css("height", $(window).height());
+    });
   };
-  updateCount();
-});
+  fullHeight();
+
+  var carousel = function () {
+    $(".home-slider").owlCarousel({
+      loop: true,
+      autoplay: true,
+      margin: 0,
+      animateOut: "fadeOut",
+      animateIn: "fadeIn",
+      nav: true,
+      dots: true,
+      autoplayHoverPause: false,
+      items: 1,
+      navText: [
+        "<span class='ion-ios-arrow-back'></span>",
+        "<span class='ion-ios-arrow-forward'></span>",
+      ],
+      responsive: {
+        0: {
+          items: 1,
+        },
+        600: {
+          items: 1,
+        },
+        1000: {
+          items: 1,
+        },
+      },
+    });
+  };
+  carousel();
+})(jQuery);
